13d46dcbb4bb803548b279b171741b46ae61d14f
mohamedayari1 <ayarim781@gmail.com>
2026-04-08 12:59:18 +0100

    Implement reference counting for Snek objects and memory management

--- src/snekobject.c
+++ src/snekobject.c
@@ -2,8 +2,52 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+void refcount_inc(snek_object_t *obj) {
+    if (obj != NULL) {
+        obj->refcount++;
+    }
+}
+
+void refcount_dec(snek_object_t *obj) {
+    if (obj != NULL && obj->refcount > 0) {
+        obj->refcount--;
+    }
+    if (obj != NULL && obj->refcount == 0) {
+        // Free the object based on its kind
+        switch (obj->kind) {
+            case INTEGER:
+                break;
+            case FLOAT:
+                // No additional memory to free
+                break;
+
+            case STRING:
+                free(obj->data.v_string);
+                break;
+        //     case VECTOR3:
+        //         refcount_dec(obj->data.v_vector3.x);
+        //         refcount_dec(obj->data.v_vector3.y);
+        //         refcount_dec(obj->data.v_vector3.z);
+        //         break;
+        //     case ARRAY:
+        //         for (size_t i = 0; i < obj->data.v_array.size; i++) {
+        //             refcount_dec(obj->data.v_array.elements[i]);
+        //         }
+        //         free(obj->data.v_array.elements);
+        //         break;
+        // }
+        free(obj);
+    }
+}
+
 snek_object_t *_new_snek_object() {
-  // ?
+  snek_object_t *obj = calloc(1, sizeof(snek_object_t));
+  if (obj == NULL) {
+    return NULL;
+  }
+  obj->refcount = 1;
+  return obj;
 }
 
 // don't touch below this line
